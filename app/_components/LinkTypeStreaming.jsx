1703a22af56783125a9a78a5f872db33cda9efad
granjerolucas <56519538+granjerolucas@users.noreply.github.com>
2024-10-26 16:38:08 -0500

    set hrefs

--- app/_components/LinkTypeStreaming.jsx
+++ app/_components/LinkTypeStreaming.jsx
@@ -1,29 +1,32 @@
 import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 
 const LinkTypeStreaming = ({ title, url, description }) => {
   return (
-    <div className=" w-32 relative flex flex-col gap-2 ">
-      <div className="bg-primary h-48 flex justify-center items-center">
-        <div className="absolute">
-          <Image
-            src={"/assets/placeholder.png"}
-            alt={title}
-            width={200}
-            height={300}
-            className="object-cover h-28"
-          />
+    <Link href={url}>
+      <div className=" w-32 relative flex flex-col gap-2 ">
+        <div className="bg-primary h-48 flex justify-center items-center">
+          <div className="absolute">
+            <Image
+              src={"/assets/placeholder.png"}
+              alt={title}
+              width={200}
+              height={300}
+              className="object-cover h-28"
+            />
+          </div>
+          <div className="">
+            <h1 className="text-white text-2xl uppercase font-semibold ">
+              {title}
+            </h1>
+          </div>
         </div>
-        <div className="">
-          <h1 className="text-white text-2xl uppercase font-semibold ">
-            {title}
-          </h1>
+        <div>
+          <p className=" text-sm">{description}</p>
         </div>
       </div>
-      <div>
-        <p className=" text-sm">{description}</p>
-      </div>
-    </div>
+    </Link>
   );
 };
 
