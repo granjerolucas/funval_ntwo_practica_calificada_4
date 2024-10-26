46951be26fac12d114d283c6ef9bfbd992fc1da4
granjerolucas <56519538+granjerolucas@users.noreply.github.com>
2024-10-26 14:21:13 -0500

    Footer section

--- app/_components/Footer.jsx
+++ app/_components/Footer.jsx
@@ -3,18 +3,22 @@ import React from "react";
 
 function Footer() {
   return (
-    <div className="bg-slate-900 text-white">
-      <div className="container mx-auto">
-        <ul className="flex justify-start items-center gap-4 py-4">
-          <li>Home</li>
-          <li>Terms and Conditions</li>
-          <li>Privacy Policy</li>
-          <li>Collection Statement</li>
-          <li>Help</li>
-          <li>Manage Account</li>
-        </ul>
-        <p>Copyriht © 2024 Funval. Práctica 4 - All rights reserved</p>
-        <div className="flex justify-between items-center gap-4 py-4">
+    <div className="bg-primary text-white py-10">
+      <div className="container mx-auto ">
+        <div className="mb-12">
+          <ul className="text-gray-200 flex justify-start items-center gap-4 py-4">
+            <li>Home</li>
+            <li>Terms and Conditions</li>
+            <li>Privacy Policy</li>
+            <li>Collection Statement</li>
+            <li>Help</li>
+            <li>Manage Account</li>
+          </ul>
+          <p className="text-gray-400">
+            Copyright © 2024 Funval. Práctica 4 - All rights reserved
+          </p>
+        </div>
+        <div className="flex my-2 justify-between items-center gap-4 py-4">
           <div className="flex gap-4 [&>span>img]:w-10 [&>span>img]:h-8">
             <span>
               <Image
